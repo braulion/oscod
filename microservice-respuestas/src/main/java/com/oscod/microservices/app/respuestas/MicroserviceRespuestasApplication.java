1e2b1bcf3915ec2362e9ff806e0b018650078a15
Braulio Chavez Miranda <bchavez@ext.vectoritcgroup.com>
2020-07-27 17:07:16 -0500

    Se agrega microservicio de respuestas

--- microservice-respuestas/src/main/java/com/oscod/microservices/app/respuestas/MicroserviceRespuestasApplication.java
+++ microservice-respuestas/src/main/java/com/oscod/microservices/app/respuestas/MicroserviceRespuestasApplication.java
@@ -2,10 +2,14 @@ package com.oscod.microservices.app.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({ "com.oscod.microservices.app.respuestas.models.entity",
+		"com.oscod.microservices.commons.alumnos.models.entity",
+		"com.oscod.microservices.commons.examenes.models.entity" })
 public class MicroserviceRespuestasApplication {
 
 	public static void main(String[] args) {
